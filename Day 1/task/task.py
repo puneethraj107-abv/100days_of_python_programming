69262cfcad80f1e44e23a1853361a32bc4908b89
Puneeth R <puneethraj107@gmail.com>
2026-02-18 19:59:34 +0530

    print function

---
+++ Day 1/task/task.py
@@ -0,0 +1,8 @@
+# Write your code below this line 👇
+print("hello world")
+print("final time ?")
+print("- FINAL TIME\nhello world")
+
+print("hello world\nhello world\nhello world")
+print("hello"+" "+"Puneeth")
+print("I'm tired of writing ,hello world")
