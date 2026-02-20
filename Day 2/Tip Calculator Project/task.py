ff5fb67dd5a5774bda500b189c219d309c57b4cb
Puneeth R <puneethraj107@gmail.com>
2026-02-20 13:07:16 +0530

    data type & tip calculator

--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -3,4 +3,7 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 
-
+total_bill=(bill/100)*tip+bill
+split_amount=total_bill/people
+print(total_bill)
+print(f"each person has to pay ${split_amount}")
