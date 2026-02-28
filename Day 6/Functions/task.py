0e077555c7f3ac49404d40d099fbbb19128866b7
Puneeth R <puneethraj107@gmail.com>
2026-02-28 15:40:55 +0530

    defining functions and calling them

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,13 @@
+def hello():
+    print("hello world, puneeth")
+
+hello()
+
+def adding(num1,num2):
+    sum=num1+num2
+    print(sum)
+
+number1=int(input("enter the number"))
+number2=int(input("enter the number"))
+
+adding(number1,number2)
