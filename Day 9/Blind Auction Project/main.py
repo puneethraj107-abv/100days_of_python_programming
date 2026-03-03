5d38688908a4e9e18c6bf94fc4581e97b421b9aa
Puneeth R <puneethraj107@gmail.com>
2026-03-03 20:25:02 +0530

    adding items to dictionary, looping and comparing values in a dictionary and finding the highest value

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,23 @@
-# TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
+from art import logo
+print(logo)
+auction={}
+
+want_to_continue=True
+while want_to_continue:
+    name=input("what is your name ")
+    bid=int(input("what is your bid "))
+    auction[name]=bid
+    continuation=input("anyone else want to place their bid")
+    print("\n"*100)
+    if continuation == "n":
+        want_to_continue=False
+print(auction)
+highest_bid=0
+highest_bidder=""
+for key in auction:
+    if auction[key] >highest_bid:
+        highest_bid=auction[key]
+        highest_bidder=key
+print(f"the highest bid is {highest_bid}, and the winner is {highest_bidder}")
 
 
--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,10 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.",
+                          "Function": "A piece of code that you can easily call over and over again."}
+print(programming_dictionary)
+programming_dictionary["Loop"]="a function that repeats a set of instructions again and again"
+print(programming_dictionary["Loop"])
+
+travel_log={"france":["lille","paris","dijion"],
+            "germany":["stuttgart","berlin"]}
+print(travel_log["france"][0])
+print(travel_log)
