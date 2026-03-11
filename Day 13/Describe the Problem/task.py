6f184792bd13e623597420c4dd5f90d2d1cd4e19
Puneeth R <puneethraj107@gmail.com>
2026-03-11 20:57:37 +0530

    debugging session

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -1,5 +1,5 @@
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 21):
         if i == 20:
             print("You got it")
 
@@ -7,6 +7,10 @@ def my_function():
 my_function()
 
 # Describe the Problem - Write your answers as comments:
+#-- print statment should have been executed when i becomes equal to 20
 # 1. What is the for loop doing?
+#-- the loop starts from i=1 and ends at i=19
 # 2. When is the function meant to print "You got it"?
+#-- when i=20
 # 3. What are your assumptions about the value of i?
+#-- i ends at 19 and never reaches 20
--- Day 13/Fix the Errors/task.py
+++ Day 13/Fix the Errors/task.py
@@ -1,3 +1,8 @@
-age = int(input("How old are you?"))
+try:
+    age = int(input("How old are you?"))
+except ValueError:
+    print("please retry entering an integer for age")
+    age=int(input("how old are you?"))
+
 if age > 18:
-print("You can drive at age {age}.")
+    print("You can drive at age {age}.")
